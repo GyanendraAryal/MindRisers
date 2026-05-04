0503aabb335148dc9dc94b873300bc87e39ef1d7
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-04 08:22:58 +0545

    added javascript practice folder

---
+++ JavaScript/variable.js
@@ -0,0 +1,15 @@
+name = 'ram'
+address = 'balaju'
+isPaid = true
+mode = 'online'
+firstName = 'hello'
+lastName = 'world'
+// product = keyboard
+fullName = firstName + lastName
+
+console.log(name);
+console.log(address);
+console.log(isPaid);
+console.log(mode);
+// console.log(product);
+console.log(fullName);
