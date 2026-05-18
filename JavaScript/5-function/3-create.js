ac28f18d61fe7bdb469ee4b125012f9eca9f548c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-18 07:10:24 +0545

    about optional chaining and own push function

--- JavaScript/5-function/3-create.js
+++ JavaScript/5-function/3-create.js
@@ -1,20 +1,40 @@
+// let users = [];
+// function addNewUser(name, age, address) {
+//     return users.push(
+//         {
+//             name: name,
+//             age: age,
+//             address: address
+//         }
+//     )
+// }
+
+// addNewUser("ram", 21, "kapan");
+// addNewUser("shyam", 22, "bagbazar");
+// addNewUser("hari", 22, "jamal");
+// console.log(users)
+
+
 let users = [];
+let count = users.length
+// console.log(count);
+
+// let count = 0
 function addNewUser(name, age, address) {
-    return users.push(
-        {
-            name: name,
-            age: age,
-            address: address
-        }
-    )
+    // users[users.length] = { name, age, address, }
+    users[count] = { name, age, address, }
+    
+    // return count = users.length
+    // console.log(count);
+    return count = users.length
 }
+// console.log(count);
 
 addNewUser("ram", 21, "kapan");
 addNewUser("shyam", 22, "bagbazar");
 addNewUser("hari", 22, "jamal");
-
-
 console.log(users)
+
 /* 
     EXPECTED-OUTPUT:
     [
