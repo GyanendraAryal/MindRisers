f454c731a56976da0dd0cb080277c1d1e2ed6d05
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-19 09:38:46 +0545

    Add string capitalization utility function example

--- JavaScript/5-function/26-capitalizeFirstLetter.js
+++ JavaScript/5-function/26-capitalizeFirstLetter.js
@@ -6,11 +6,17 @@ capitalizeFirstLetter("hello") ➞ "Hello"
 capitalizeFirstLetter("world") ➞ "World"
 capitalizeFirstLetter("javascript") ➞ "Javascript"
 */
-function capitalizeFirstLetter(str) {
-    // your code here
-    // console.log(str);
-    let capitalize = str.charAt(0).toUpperCase() + str.slice(1)
-    return capitalize
-}
+// function capitalizeFirstLetter(str) {
+//     // your code here
+//     // console.log(str);
+//     let capitalize = str.charAt(0).toUpperCase() + str.slice(1)
+//     return capitalize
+// }
 
-console.log(capitalizeFirstLetter("hello"));
+// console.log(capitalizeFirstLetter("hello"));
+
+// let capitalize;
+const capitalizeFirstLetter = (input) => {
+    return capitalize = input.charAt(0).toUpperCase() + input.slice(1)
+}
+console.log(capitalizeFirstLetter("hello"))
