0b47e9a374b79afa7b9efc30f1961625614e507d
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-31 21:55:54 +0545

    Update main entry point

--- React/chaitra-base-react/src/main.jsx
+++ React/chaitra-base-react/src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import Courses from "./components/Courses";
 import './index.css'
+import Blogs from "./components/Blogs";
 // import App from "./App";
 
 createRoot(document.getElementById("root")).render(
     // <App/>
-    <Courses />
+    <>
+        <Courses />
+        <Blogs />
+    </>
 );
