8459722fb7a9d5a3ce617a3c1a375b590acce047
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-31 21:55:54 +0545

    Update source data

--- React/chaitra-base-react/src/data.js
+++ React/chaitra-base-react/src/data.js
@@ -6,6 +6,16 @@ let courses = [
   },
   {
     featured: true,
+    title: "MERN Stack Training in Nepal",
+    image: "/static/Images/courses/banner/mern-banner-min_nplbrgN.jpg",
+  },
+  {
+    featured: true,
+    title: "MERN Stack Training in Nepal",
+    image: "/static/Images/courses/banner/mern-banner-min_nplbrgN.jpg",
+  },
+  {
+    featured: true,
     title: "Python with Django Training in Nepal",
     image: "/static/Images/courses/banner/python-banner-min_7hCoIdR.jpg",
   },
@@ -37,4 +47,68 @@ let courses = [
   },
 ];
 
-export default courses
+let blogs = [
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Web Designing Training in Putalisadak, Kathmandu",
+    image:
+      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "Graphic Designing Training in Putalisadak, Kathmandu | Mindrisers",
+    image:
+      "/static/Images/blog/single_post/Graphic_Designing_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
+  },
+  {
+    title:
+      "Data Science with Python Training in Putalisadak, Kathmandu | Mindrisers",
+    image:
+      "/static/Images/blog/single_post/Data_Science_with_Python_Training_in_Putalisadak_Kathmandu.jpg",
+  },
+  {
+    title: "UI/UX Training in Putalisadak, Kathmandu | Mindrisers",
+    image:
+      "/static/Images/blog/single_post/UIUX_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
+  },
+  {
+    title: "Flutter Training in Putalisadak, Kathmandu | Mindrisers",
+    image:
+      "/static/Images/blog/single_post/Flutter_Training_in_Putalisadak_Kathmandu_Mindrisers.png",
+  },
+  {
+    title: "DevOps Training in Putalisadak, Kathmandu | Mindrisers",
+    image:
+      "/static/Images/blog/single_post/DevOps_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
+  },
+];
+
+export const data = { courses, blogs };
