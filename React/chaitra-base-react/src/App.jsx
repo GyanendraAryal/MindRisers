875901f7386e71be88f4d670bbfb92e8c9930a4c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-31 21:55:54 +0545

    Update App layout and structure

--- React/chaitra-base-react/src/App.jsx
+++ React/chaitra-base-react/src/App.jsx
@@ -6,39 +6,7 @@ function App() {
     "Lorem ipsum dolor sit amet consectsequi quas fugit incidunt.";
 
   let domain = "https://mindrisers.com.np";
-  let blogs = [
-    {
-      title: "Web Designing Training in Putalisadak, Kathmandu",
-      image:
-        "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
-    },
-    {
-      title: "Graphic Designing Training in Putalisadak, Kathmandu | Mindrisers",
-      image:
-        "/static/Images/blog/single_post/Graphic_Designing_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-    },
-    {
-      title:
-        "Data Science with Python Training in Putalisadak, Kathmandu | Mindrisers",
-      image:
-        "/static/Images/blog/single_post/Data_Science_with_Python_Training_in_Putalisadak_Kathmandu.jpg",
-    },
-    {
-      title: "UI/UX Training in Putalisadak, Kathmandu | Mindrisers",
-      image:
-        "/static/Images/blog/single_post/UIUX_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-    },
-    {
-      title: "Flutter Training in Putalisadak, Kathmandu | Mindrisers",
-      image:
-        "/static/Images/blog/single_post/Flutter_Training_in_Putalisadak_Kathmandu_Mindrisers.png",
-    },
-    {
-      title: "DevOps Training in Putalisadak, Kathmandu | Mindrisers",
-      image:
-        "/static/Images/blog/single_post/DevOps_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-    },
-  ];
+  
 
   return (
     <>
