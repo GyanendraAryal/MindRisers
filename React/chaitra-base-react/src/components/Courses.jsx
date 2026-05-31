0347fb5143bb3214b8d1e4c0528c5d00a76b4dee
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-31 21:55:54 +0545

    Refactor Courses component

--- React/chaitra-base-react/src/components/Courses.jsx
+++ React/chaitra-base-react/src/components/Courses.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
-import data from '../data'
+import {data} from '../data'
 
-
-// console.log(data);
+const {courses} = data
+// console.log(courses);
 
 function Courses() {
     let domain = "https://mindrisers.com.np";
     return (
         <>
             <h1 className='h-15 text-center mt-6 text-2xl font-semibold'>Featured Items</h1>
-            <div className='min-h-screen w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-6 p-4 bg-white'>
-                {data.map((item, index) => {
+            <div className='h-full justify-items-center gap-4 w-full grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                {courses.map((item, index) => {
                     return (
-                        <div key={index} className='flex flex-col items-center justify-between h-72 w-full max-w-[240px] p-4 rounded-lg shadow-md bg-green-400'>
-                            <li className='list-none font-medium text-center order-2'>{item.title}</li>
-                            <li className='list-none w-full h-40 flex items-center justify-center overflow-hidden rounded-md bg-white order-1'>
-                                <img className='max-h-full max-w-full object-contain' src={`${domain}${item.image}`} alt="items" />
-                            </li>
+                        <div key={index} className='flex flex-col text-white item-center justify-evenly h-72 w-full max-w-60 rounded-lg shadow-md bg-green-500'>
+                            <li className='list-none font-medium text-center'>{item.title}</li>
+                            <li className='list-none font-medium text-center'><img src={`${domain}${item.image}`} alt="item" /></li>
                         </div>
                     )
                 })}
