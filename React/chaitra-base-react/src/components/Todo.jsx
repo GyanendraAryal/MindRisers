a14a467d9eacece8e2a511a9081dc4f03d026149
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-05 15:36:24 +0545

    Improve Todo component functionality

--- React/chaitra-base-react/src/components/Todo.jsx
+++ React/chaitra-base-react/src/components/Todo.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { todos } from '../data/todos'
+import Button from './Button'
 function Todo() {
     return (
         <>
             <table className='w-full border text-center' >
                 <thead>
                     <tr>
-                        <th className='border-2'>Todos</th>
-                        <th className='border-2'>Status</th>
+                        <th className='border-2 bg-blue-500'>Todos</th>
+                        <th className='border-2 bg-blue-500'>Status</th>
+                        <th className='border-2 bg-blue-500'>Settings</th>
+
                     </tr>
                 </thead>
                 <tbody>
                     {todos.map((item) => {
                         return <tr key={item.id}>
-                            <td className='border-2'>{item.todo}</td>
-                            <td className='border-2'>{item.completed ? "Completed" : "Pending"}</td>
+                            <td className={`border-2`}>{item.todo}</td>
+                            <td className={`border-2`}><span className={` p-1 text-sm m-0.1 ${item.completed ? "bg-green-500" : "bg-red-500"}`}>{item.completed ? "Completed" : "Pending"}</span></td>
+                            <td className='border-2 flex items-center justify-center'><Button label="edit" size="small" /><Button label="delete" size="small" /></td>
                         </tr>
-
                     })}
                 </tbody>
             </table>
