0e1102e9cb14813342703af2fb3996b753cd917d
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-09 07:47:47 +0545

    Add All component to aggregate search features

---
+++ React/chaitra-base-react/src/components/All.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+
+function All() {
+  return (
+    <>
+      <div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+        <div>All All All All All All All All All All All All All All</div>
+      </div>
+      <br/>
+      <br/>
+
+      <div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+      </div>
+
+      <br/>
+      <br/>
+
+      <div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default All
