6009aa40206cc8b0627ef0e69177c12d7b2f3315
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-09 07:47:43 +0545

    Add Videos playback component

---
+++ React/chaitra-base-react/src/components/Videos.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Videos() {
+  return (
+    <div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+        <div>
+          Videos Videos Videos Videos Videos Videos Videos Videos Videos Videos
+          Videos Videos Videos Videos
+        </div>
+      </div>
+  )
+}
+
+export default Videos
