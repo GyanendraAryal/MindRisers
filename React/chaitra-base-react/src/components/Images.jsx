d4e95483e2d8861917dccb593d36c0295dbaf7a8
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-09 07:47:37 +0545

    Add Images display component

---
+++ React/chaitra-base-react/src/components/Images.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Images() {
+  return (
+    <div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+        <div>
+          Images Images Images Images Images Images Images Images Images Images
+          Images Images Images Images
+        </div>
+      </div>
+  )
+}
+
+export default Images
