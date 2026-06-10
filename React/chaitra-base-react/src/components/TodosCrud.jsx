b1ab20f2820f7f1e48cd8fd2ba87938d83a4a741
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-10 14:20:36 +0545

    crud todos

--- React/chaitra-base-react/src/components/TodosCrud.jsx
+++ React/chaitra-base-react/src/components/TodosCrud.jsx
@@ -1,53 +1,120 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
+import { useEffect } from 'react'
 
 function TodosCrud() {
 
-  const handleSubmmit = (e) => {
+  const [editId, setEditId] = useState(null)
+  const [editText, setEditText] = useState('')
+
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState(() => {
+    const todos = localStorage.getItem('todos')
+    return todos ? JSON.parse(todos) : []
+  })
+
+  useEffect(() => {
+    if (!todos) return
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  //Handling Submit
+  const handleSubmit = (e) => {
     e.preventDefault()
-    if (!input.trim()) return
-    const newTodo = {
+    if (!input) return
+    let newTodo = {
       id: Date.now(),
-      title: input
+      title: input,
+      isChecked: false
     }
+
     setTodos(prevTodos => [...prevTodos, newTodo])
     setInput('')
   }
 
+  //Handling Delete
   const handleDelete = (id) => {
     if (!id) return
-    setTodos(prevTodos => prevTodos.filter((todo) => todo.id !== id))
+    setTodos(prevTodos => prevTodos.filter((item) => item.id == id ? item.id !== id : item))
   }
 
-  const [input, setInput] = useState('')
-  const [todos, setTodos] = useState(() => {
-    const work = localStorage.getItem('todos')
-    return work ? JSON.parse(work) : []
-  })
+  //Handling Edit
+  const handleEdit = (id, currentText) => {
+    if (!id || !currentText.trim()) return
+    setEditId(id)
+    setEditText(currentText)
+  }
 
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
+  //Handling Saving
+  const handleSave = (id) => {
+    if (!id) return
+    setTodos(prevTodos => prevTodos.map((item) => item.id == id ? { ...item, title: editText } : item))
+    setEditId(null)
+    setEditText('')
+  }
+
+  //Handling Checked
+  const handleChecked = (id) => {
+    if (!id) return
+    setTodos(prevTodos => prevTodos.map((item) => item.id == id ? { ...item, isChecked: !item.isChecked } : item))
+  }
+
+
   return (
     <>
-      <form onSubmit={handleSubmmit}>
+      <form onSubmit={handleSubmit}>
         <input
-          className='border'
           value={input}
           onChange={(e) => setInput(e.target.value)}
-        />
+          className='border' />
         <Button type='submit' size='small' label='Add' />
       </form>
-      <ul>
-        {todos.map((todo) => (
-          <div key={todo.id} className='flex justify-between items-center mt-1 bg-amber-200 h-6 w-60'>
-            <li>{todo.title}</li>
-            <button className='h-6 px-2 cursor-pointer border' onClick={() => handleDelete(todo.id)}>Delete</button>
-          </div>
-        ))}
-      </ul>
+
+
+      {todos.map((items) => (
+        <div
+          key={items.id}
+          className='w-150 mt-2 flex items-center justify-between bg-amber-200'
+        >
+          {editId == items.id ? (
+            <>
+              <input
+                className='border h-5 bg-white w-40'
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+              />
+              <button
+                onClick={() => handleSave(items.id)}
+                className='h-6 cursor-pointer w-14 border bg-amber-500'>Save</button>
+            </>
+          ) : (
+            <>
+              < div className='flex items-center px-2 font-semibold justify-between h-12 w-35 bg-amber-200' >
+                <input
+                  checked={items.isChecked}
+                  onChange={() => handleChecked(items.id)}
+                  type="checkbox" />
+                <li className={`list-none text-2xl ${items.isChecked ? 'line-through bg-gray-400' : ''}`}>{items.title}</li>
+              </div >
+              <div className='flex gap-1 px-2'>
+                <button
+                  onClick={() => handleDelete(items.id)}
+                  className='h-6 cursor-pointer w-14 border bg-amber-500'>Delete</button>
+                <button
+                  onClick={() => handleEdit(items.id, items.title)}
+                  className='h-6 cursor-pointer w-14 border bg-amber-500'>Edit</button>
+              </div></>
+          )}
+
+        </div>
+      ))}
+
     </>
   )
 }
 
 export default TodosCrud
+
+
+
+
