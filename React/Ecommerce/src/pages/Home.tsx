e6ea1b613787b6b215d1114f04e7c61eb7120ae1
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-25 22:20:08 +0545

    Update home page

---
+++ React/Ecommerce/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// import Navbar from "../components/Navbar"
+// import Footer from "../components/Footer"
+
+function Home() {
+    return (
+        <>
+            <h1>Home page</h1>
+        </>
+    )
+}
+
+export default Home
