c3ecfb795bbde0f9291fff93557ab29f371568ba
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-25 22:20:08 +0545

    Configure application routing and setup

--- React/Ecommerce/src/App.tsx
+++ React/Ecommerce/src/App.tsx
@@ -1,11 +1,44 @@
-import {createBrowserRouter } from "react-router-dom"
-function App() {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Login from './pages/Login.tsx'
+import Signup from './pages/Signup.tsx'
+import { useState } from 'react'
+import Layout from './pages/Layout.tsx'
+import ProductsListing from './pages/products/ProductsListing.tsx'
 
-  return (
-    <>
-      <h1>Hello from typescript</h1>
-    </>
-  )
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout loggedIn={loggedIn} />,
+//     children: [
+//       { path: '', element: <Home /> },
+//       // Passes the state setter correctly to Login
+//       { path: 'login', element: <Login setLoggedIn={setLoggedIn} /> },
+//       { path: 'register', element: <Signup /> },
+//     ],
+//   },
+// ])
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout loggedIn={loggedIn} />,
+      children: [
+        { path: '', element: <Home /> },
+        { path: 'login', element: <Login  /> },
+        { path: 'register', element: <Signup /> },
+        {path:'products',element:<ProductsListing/>}
+      ]
+    }
+  ])
+
+  return (<RouterProvider router={router} />)
 }
 
 export default App
